818b471bc8c807dbc990347d1e2a2c222e062f2f
Fateme Ahmadi <ahmadifateme055@gmail.com>
2022-08-10 16:06:40 +0430

    پاک کردن کامنت های بلا استفاده

--- assets/js/decode.js
+++ assets/js/decode.js
@@ -1,5 +1,4 @@
-// let start = 0;
-// let numberOfAddress = 0;
+
 let len;
 let str;
 let char;
@@ -84,71 +83,3 @@ memoryTable.appendChild(ramTable);
 
 
 
-
-
-// var list = document.getElementById("list");
-// for (let i = 0; i < 5; i++) {
-//     var storedNames = JSON.parse(localStorage.getItem(`DataFile${i}`));
-//     if (storedNames == null) {} else {
-//         var tagli = document.createElement("li");
-//         tagli.classList.add("listData");
-//         tagli.innerHTML = `DataFile${i}`;
-//         list.appendChild(tagli);
-//         tagli.onclick = function () {
-//             var namelist = this.innerHTML;
-//             var inp = document.getElementsByTagName("input");
-//             var storedNames = JSON.parse(localStorage.getItem(namelist));
-//             for (let j = 1; j <= 100; j++) {
-//                 var d = j % 10;
-//                 var f = ((j - d) / 10) + 1;
-//                 var a;
-//                 if (d == 1) {
-
-//                     a = "A" + f;
-//                 } else if (d == 2) {
-//                     a = "B" + f;
-//                 } else if (d == 3) {
-
-//                     a = "C" + f;
-//                 } else if (d == 4) {
-
-//                     a = "D" + f;
-//                 } else if (d == 5) {
-
-//                     a = "E" + f;
-//                 } else if (d == 6) {
-
-//                     a = "F" + f;
-//                 } else if (d == 7) {
-
-//                     a = "G" + f;
-//                 } else if (d == 8) {
-//                     a = "H" + f;
-//                 } else if (d == 9) {
-
-//                     a = "I" + f;
-//                 } else {
-
-//                     a = "J" + f;
-//                 }
-//                 if (a == inp[j - 1].name) {
-//                     inp[j - 1].value = storedNames[j];
-//                 }
-//             }
-//         }
-//     }
-// }
-// const table = document.querySelector('.ram table');
-// const rows = table.getElementsByTagName('tr');
-// const columns = table.getElementsByTagName('td');
-
-// function updateContentsColumn() {
-//     let counter = start;
-//     console.log(numberOfAddress)
-//     for (let i = parseInt('0x' + start) + 1; i < parseInt('0x' + start) + numberOfAddress + 1; i++) {
-//         columns[i * 3 + 2].innerText = memory_table_contents[counter];
-//         counter = addHexNumbers(counter, '1');
-//     }
-//     // scrollToRow(parseInt('0x' + startAddress) + 1);
-// }
-// updateContentsColumn();
