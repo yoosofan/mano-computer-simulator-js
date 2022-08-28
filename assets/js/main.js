0684680def961ecd36d01aa696d2b963e8f7820c
Motahareh-bolandi <motahareh.bolandi1999@gmail.com>
2022-08-28 11:17:33 +0430

    اضافه کردن assembler

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ const adr = document.getElementsByClassName("Address");
 const ins = document.getElementById("insert");
 var elementt = document.getElementsByClassName("data");
 var getDataTextarea = document.getElementById("textarea");
-
+var getDataAsembler = document.getElementById("asembler");
+var instructionAssembler;
 // var emptyRegister=document.querySelectorAll(".regList");
 ins.addEventListener("click", insert);
 
@@ -18,7 +19,7 @@ function natural() {
 function insert() {
     // console.log(count, ";;;")
     var data = document.getElementById("firstInput").value;
-    if (data == "") {} else
+    if (data == "") { } else
         elementt[count - 1].innerText = data;
 
     console.log(data, "llllfjfhdjfgdj")
@@ -58,10 +59,10 @@ function store() {
             else
                 dataStore[b] = " ";
         else
-        if (b == 99)
-            dataStore[b] = tag + ",";
-        else
-            dataStore[b] = tag;
+            if (b == 99)
+                dataStore[b] = tag + ",";
+            else
+                dataStore[b] = tag;
     }
     getDataTextarea.value = dataStore;
 }
@@ -78,7 +79,7 @@ function localstore() {
     for (let i = listTest.length; i < 10; i++) {
         let o = i - 2;
         var storedNames = JSON.parse(localStorage.getItem(`DataFile${o}`));
-        if (storedNames == null) {} else {
+        if (storedNames == null) { } else {
             list[i].innerHTML = `DataFile${o}`;
             list[i].onclick = function () {
                 var namelist = this.innerHTML;
@@ -96,7 +97,7 @@ for (let i = listTest.length; i < 10; i++) {
     let o = i - 2;
     var storedNames = JSON.parse(localStorage.getItem(`DataFile${o}`));
     // console.log(storedNames, "{{{{{{{{{[")
-    if (storedNames == null) {} else {
+    if (storedNames == null) { } else {
         list[i].innerHTML = `DataFile${o}`;
         list[i].onclick = function () {
             var namelist = this.innerHTML;
@@ -128,7 +129,7 @@ function sendText() {
     let c = document.getElementsByClassName('data');
     let j = 0;
     for (let i = 0; i < c.length; i++) {
-        if (a[j] == undefined || a[j] == " ") {} else if (a[j] == "") {
+        if (a[j] == undefined || a[j] == " ") { } else if (a[j] == "") {
             break
         } else {
             console.log(a[j])
@@ -162,7 +163,7 @@ function showCPU() {
     registerHex.memory = binaryToHex(memory);
     var start = document.querySelectorAll(".cpu");
     var end = document.querySelectorAll(".endInput");
-    document.getElementById("versions").style.display="none";
+    document.getElementById("versions").style.display = "none";
     [].forEach.call(start, function (el) {
         el.classList.remove("StartCPU");
     });
@@ -188,7 +189,7 @@ function showCPU() {
 function back() {
     document.getElementById("container").style.display = "block";
     document.getElementById("HLT").classList.remove("shutDown");
-    document.getElementById("versions").style.display="block";
+    document.getElementById("versions").style.display = "block";
     var end = document.querySelectorAll(".cpu");
     var start = document.querySelectorAll(".endInput");
     [].forEach.call(start, function (el) {
@@ -200,4 +201,69 @@ function back() {
     [].forEach.call(code, function (el) {
         el.classList.remove("border");
     });
+}
+
+// TextAreaAssembler
+function assembler() {
+    var stringValu = getDataAsembler.value.split('\n');
+    var opcode;
+    var arr = [];
+    for (let index = 0; index < stringValu.length; index++) {
+        var s = 0;
+        var b = 0;
+        instructionAssembler = stringValu[index].split(' ');
+        for (let i = 0; i < memory_instructions.length; i++) {
+            var a = 0;
+            if (memory_instructions[i][0] == instructionAssembler[0].toUpperCase()) {
+                b = 1;
+                s = 1;
+                for (let j = 0; j < memoryAddress.length; j++) {
+                    if (memoryAddress[j].innerText == instructionAssembler[1]) {
+                        opcode = memory_instructions[i][2];
+                        a = 1;
+                        instructionAssembler[1] = instructionAssembler[1].slice(2, 5);
+                        console.log(instructionAssembler[1].slice(2, 5), "kkkkkkkk");
+
+                    }
+                }
+                if (a == 0)
+                    opcode = memory_instructions[i][1];
+                arr[index] = opcode + instructionAssembler[1] + ",";
+                break;
+            }
+
+        }
+        if (b == 0) {
+            b = 2;
+            for (let i = 0; i < register_instructions.length; i++) {
+                if (register_instructions[i][0] == instructionAssembler[0].toUpperCase()) {
+                    s = 1;
+                    opcode = register_instructions[i][1];
+                    arr[index] = opcode + ",";
+                    break;
+                }
+
+            }
+            if (s == 0) {
+                for (let i = 0; i < memoryAddress.length; i++) {
+                    if (instructionAssembler[0] == memoryAddress[i].innerText) {
+                        // var ind=memoryAddress.indexOf(memoryAddress[i]);
+                        arr[i] = instructionAssembler[1] +",";
+                    }
+
+                }
+            }
+        }
+
+    }
+    for (let h = 0; h < memoryAddress.length; h++) {
+        console.log(arr[h],";ydhdjhdjnhgjhnh")
+        if (arr[h] == undefined) {
+            arr[h] = " ,";
+
+        }
+    }
+    console.log(arr, "\n", arr.length, "llllllllllllllllllllllllllll")
+    arr = arr.join('');
+    getDataTextarea.value = arr;
 }
